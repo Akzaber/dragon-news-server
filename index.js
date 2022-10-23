847581c9683422fe75f068941041e0fed3b37233
Akzaber <zaberkhan611@gmail.com>
2022-10-23 16:51:52 +0600

    newses data

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const port = process.env.PORT || 5000;
 
 app.use(cors())
 
-const categories = require('./Data/categories.json')
+const categories = require('./Data/categories.json');
+const news = require('./Data/news.json');
 
 app.get('/', (req, res) => {
   res.send('Dragon Api Running')
@@ -13,6 +14,27 @@ app.get('/', (req, res) => {
 
 app.get('/news-category', (req, res) => {
   res.send(categories);
+})
+
+app.get('/news/:id', (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find(n => n._id === id)
+  res.send(selectedNews);
+})
+
+app.get('/category/:id', (req, res) => {
+  const id = req.params.id;
+  if(id === "08"){
+    res.send(news);
+  }
+  else{
+    const newsCategory = news.filter(n => n.category_id === id)
+    res.send(newsCategory);
+  }
+})
+
+app.get('/news', (req, res) => {
+  res.send(news);
 })
 
 app.listen(port, () => {
